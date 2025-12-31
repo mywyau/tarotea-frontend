d4b7f98b5f21c3b159a63ec38f1009fde9ee5ddc
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-12-31 22:43:54 +0000

    wip

--- server/api/words/[id].ts
+++ server/api/words/[id].ts
@@ -6,7 +6,7 @@ export default defineEventHandler(async (event) => {
   const id = getRouterParam(event, 'id')
 
   try {
-    const file = join(process.cwd(), 'server/data/words', `${decodedParam}.json`)
+    const file = join(process.cwd(), 'server/data/words', `${id}.json`)
     const json = await readFile(file, 'utf-8')
     console.log(file)
     return JSON.parse(json)
