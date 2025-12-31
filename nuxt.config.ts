95600c06ed96c19f3c7b71e18a1af8e22ade049f
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-12-31 17:52:39 +0000

    wip comment out nuxt image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/fonts", "@nuxt/image", "@nuxtjs/tailwindcss"],
+  modules: [
+    "@nuxt/fonts",
+    // "@nuxt/image",
+    "@nuxtjs/tailwindcss"
+    ],
 
   nitro: {
     preset: "vercel",
