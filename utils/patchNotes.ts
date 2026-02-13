e3ac8223759d7c3b14c08b5559767860effc30be
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-02-13 00:24:52 +0000

    update patch notes

--- utils/patchNotes.ts
+++ utils/patchNotes.ts
@@ -1,30 +1,41 @@
 export interface PatchNote {
-  date: string
-  title?: string
-  items: string[]
+  date: string;
+  title?: string;
+  items: string[];
 }
 
 export const patchNotes: PatchNote[] = [
   {
-    date: 'Feb 2026',
-    title: 'February Dev Notes',
+    date: "Feb 2026",
+    title: "Upcoming",
     items: [
-      'Adding exercise quizzes and exercises section as placeholder for possibly new learning modes',
-      'Adding level 1 sentence based exercises',
-      'Adding topics placeholder for future content',
-      'Added new level 4 vocabulary, audio and quizzes',
-      'Auth still locked',
-    ]
+      "More content for topics 'time & dates' & 'money'",
+      "More content for level 5 & 6",
+      "Sentence based topic quiz to have a larger sentence pool, instead of just 20 questions",
+      "",
+      "",
+    ],
   },
   {
-    date: 'Jan 2026',
-    title: 'Create app and add content, quizzes and audio',
+    date: "Feb 2026",
+    title: "February Dev Notes",
     items: [
-      'Introduced word-only based quizzes',
-      'Introduced audio-only based quizzes',
-      'Fixed and improved mobile viewing',
-      'Added level 1-3 word tiles',
-      'Added Audio and setence examples',
-    ]
-  }
-]
+      "Added topics 5 topics, 4 free topics, 1 paid",
+      "Adding quizzes for topics. Vocabulary, word only sentences and word audio.",
+      "Added new level 4 vocabulary, audio and quizzes",
+      "Adding topics placeholder for future content",
+      "Auth still locked",
+    ],
+  },
+  {
+    date: "Jan 2026",
+    title: "Create app and add content, quizzes and audio",
+    items: [
+      "Introduced word-only based quizzes",
+      "Introduced audio-only based quizzes",
+      "Fixed and improved mobile viewing",
+      "Added level 1-3 word tiles",
+      "Added Audio and setence examples",
+    ],
+  },
+];
