4928dc7a8ea1eb13ace4a8a95053a5008e3bf86b
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-03-10 15:07:40 +0000

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -35,7 +35,7 @@ function toggleTrainingMode() {
       </h1>
 
       <p class="mt-4 text-lg text-gray-700 max-w-xl mx-auto">
-        Structured vocabulary, audio practice, and quizzes designed to help your cantonese studying journey.
+        Structured vocabulary, audio practice, and quizzes designed to help you on your cantonese studying journey.
       </p>
 
       <NuxtLink to="/topics" class="mt-8 inline-block font-semibold px-8 py-3 rounded-lg
