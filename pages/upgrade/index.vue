c8234b1f91dd1aa639358ea15bdaf6dd7f15dcec
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-03-28 23:06:39 +0000

    improve wording of upgrade page

--- pages/upgrade/index.vue
+++ pages/upgrade/index.vue
@@ -54,13 +54,13 @@ function upgrade(plan: 'monthly' | 'yearly') {
 
         <!-- Benefits -->
         <ul class="text-left text-gray-700 space-y-2 max-w-sm mx-auto">
-          <li>• Full access to all content</li>
-          <li>• 3000 Echo Lab requests per month</li>
-          <li>• Randomised exercises and tests</li>
+          <li>• Learn faster with full access to all content</li>
+          <li>• Practice and check your pronuciation with 3000 Echo Lab requests per month</li>
+          <li>• Check your recall and understanding with randomised exercises and tests</li>
           <li>• High quality audio for every word</li>
-          <li>• Neatly organised structured content</li>
-          <li>• XP tracking for all words</li>
-          <li>• Access new topics, words, features added regularly</li>
+          <li>• Neatly organised and structured content</li>
+          <li>• Practice less familiar and weakest words</li>
+          <li>• Access new topics, words and features added regularly</li>
         </ul>
 
         <!-- Plans -->
