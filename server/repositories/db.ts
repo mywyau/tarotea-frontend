2b032d89ca1c91b09138ba744df75216cb214a41
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-04-03 23:34:41 +0100

    wip

--- server/repositories/db.ts
+++ server/repositories/db.ts
@@ -6,7 +6,7 @@ const { Pool } = pg;
 export const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-  max: 3,
+  max: 6,
   idleTimeoutMillis: 10000,
   connectionTimeoutMillis: 2000,
 });
