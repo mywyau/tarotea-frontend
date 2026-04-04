124d0dc5a69a1e7b88b06703e75923ee30b8eabe
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-04-04 01:07:06 +0100

    clean up comment

--- pages/dojo/level/jyutping/training/[slug]/v2/index.vue
+++ pages/dojo/level/jyutping/training/[slug]/v2/index.vue
@@ -593,11 +593,6 @@ watch(
 
 <template>
   <main class="mx-auto max-w-xl px-6 pt-12 pb-28 sm:pb-12">
-    <!-- <div class="mb-6">
-      <NuxtLink :to="`/dojo/level/`" class="text-black text-sm hover:underline">
-        ← Back to Level Dojo
-      </NuxtLink>
-    </div> -->
 
     <div class="mb-6">
       <BackLink />
