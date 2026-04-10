b7f73475bed5565d6276c5ca512d9d2f2f8ba23f
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-04-10 22:00:59 +0100

    wip

--- pages/writing/[category]/sentences/[id]/[idx].vue
+++ pages/writing/[category]/sentences/[id]/[idx].vue
@@ -237,7 +237,7 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="mx-auto max-w-4xl px-3 py-5 sm:px-6 sm:py-8 lg:px-8">
+    <div v-if="ready" class="mx-auto max-w-4xl px-3 py-5 sm:px-6 sm:py-8 lg:px-8">
         <div class="mb-4 sm:mb-6">
             <BackLink />
         </div>
