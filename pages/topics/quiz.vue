6b206294921823de3bfba28d3ac0436bd56c5152
Michael <47248104+michael-hmrc@users.noreply.github.com>
2026-04-18 16:32:38 +0100

    Add modern SaaS homepage improvement plan

--- pages/topics/quiz.vue
+++ pages/topics/quiz.vue
@@ -151,13 +151,13 @@ onMounted(async () => {
                         Audio Only
                     </NuxtLink> -->
 
-                    <NuxtLink :to="canEnterTopic(topic) ? `/topic/quiz/sentences/no-audio/${topic.id}/v3` : undefined"
+                    <NuxtLink :to="canEnterTopic(topic) ? `/topic/quiz/sentences/no-audio/${topic.id}/v3/start-quiz` : undefined"
                         class="topic-btn topic-btn-yellow"
                         :class="{ 'pointer-events-none opacity-60': topic.comingSoon }">
                         Sentences
                     </NuxtLink>
 
-                    <NuxtLink :to="canEnterTopic(topic) ? `/topic/quiz/sentences/audio/${topic.id}/v3` : undefined"
+                    <NuxtLink :to="canEnterTopic(topic) ? `/topic/quiz/sentences/audio/${topic.id}/v3/start-quiz` : undefined"
                         class="topic-btn topic-btn-blush"
                         :class="{ 'pointer-events-none opacity-60': topic.comingSoon }">
                         Audio Only Sentences
