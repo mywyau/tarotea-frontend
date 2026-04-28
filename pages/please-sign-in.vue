746bb5a4487e16601f8ae4a3956052adf3395e23
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-04-28 03:49:05 +0100

    wip

--- pages/please-sign-in.vue
+++ pages/please-sign-in.vue
@@ -15,22 +15,30 @@ definePageMeta({
     </div>
 
     <!-- Title -->
-    <div class="space-y-4">
+    <div class="space-y-8">
       <h1 class="text-3xl font-semibold">Please sign in 🚧</h1>
 
       <p class="text-gray-600 text-sm max-w-md mx-auto">
         Sign in to view this content and continue your journey.
       </p>
+
+
     </div>
 
     <!-- CTA -->
-    <div class="pt-4">
+    <div class="pt-2">
       <button type="button"
         class="inline-block px-8 py-3 rounded-lg font-semibold shadow-md hover:shadow-lg transition hover:-translate-y-0.5"
-        style="background-color:#EAB8E4;" @click="login()">
+        style="background-color:rgba(88,199,95,0.45)" @click="login()">
         Sign in / Create account →
       </button>
+
+      <p class="text-gray-600 text-sm mt-10 max-w-md mx-auto">
+        If you are struggling to sign in please try clearing cookies and refreshing the page
+      </p>
     </div>
+
+
 
   </main>
 </template>
