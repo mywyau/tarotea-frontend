4e2ca05d1aa4021ebd31a54fe97e4940e44fcb43
Michael <47248104+michael-hmrc@users.noreply.github.com>
2026-05-10 13:39:57 +0100

    Standardize back link row layout

--- components/BackLink.vue
+++ components/BackLink.vue
@@ -16,14 +16,36 @@ const goBack = useGoBack(props.fallback)
 </script>
 
 <template>
-  <component
-    :is="props.to ? NuxtLink : 'button'"
-    :to="props.to"
-    :type="props.to ? undefined : 'button'"
-    class="inline-flex items-center gap-1.5 text-sm text-black hover:underline bg-transparent border-0 p-0 cursor-pointer"
-    @click="props.to ? undefined : goBack()"
-  >
-    <ArrowLeft class="h-4 w-4" aria-hidden="true" />
-    <span>{{ label }}</span>
-  </component>
+  <nav class="back-link-row" aria-label="Page navigation">
+    <div class="back-link-row__inner">
+      <component
+        :is="props.to ? NuxtLink : 'button'"
+        :to="props.to"
+        :type="props.to ? undefined : 'button'"
+        class="inline-flex items-center gap-1.5 text-sm text-black hover:underline bg-transparent border-0 p-0 cursor-pointer"
+        @click="props.to ? undefined : goBack()"
+      >
+        <ArrowLeft class="h-4 w-4" aria-hidden="true" />
+        <span>{{ label }}</span>
+      </component>
+    </div>
+  </nav>
 </template>
+
+<style scoped>
+.back-link-row {
+  --back-link-row-max-width: 64rem;
+
+  display: block;
+  inline-size: 100vw;
+  margin-inline: calc(50% - 50vw);
+  text-align: start;
+}
+
+.back-link-row__inner {
+  inline-size: 100%;
+  max-inline-size: var(--back-link-row-max-width);
+  margin-inline: auto;
+  padding-inline: 1rem;
+}
+</style>
