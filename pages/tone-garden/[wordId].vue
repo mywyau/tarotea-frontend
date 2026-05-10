7a841a738b0374e567ea5d2b37f2e0258153c81e
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-05-10 19:37:39 +0100

    make tone garden bigger

--- pages/tone-garden/[wordId].vue
+++ pages/tone-garden/[wordId].vue
@@ -445,9 +445,10 @@ async function runToneCheck() {
 </script>
 
 <template>
+
   <div class="min-h-screen text-gray-900">
-    <div class="mx-auto max-w-3xl px-4 py-10">
-      <!-- <BackLink /> -->
+
+    <div class="mx-auto max-w-4xl px-4 py-10">
 
       <h1 class="text-3xl font-bold">Tone Garden</h1>
 
